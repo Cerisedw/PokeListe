d736bc65dd1a5fda6d10547959466b6214339df3
CeriseDW <CarnetsDeCerise@gmail.com>
2020-02-04 16:58:52 +0100

    tentative de fix probleme orderby part1

---
+++ PokeListe.Tools/Mappers/InfoPokemonLocTools.cs
@@ -0,0 +1,35 @@
+﻿using PokeListe.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeListe.Tools.Mappers
+{
+    public static class InfoPokemonLocTools
+    {
+
+        public static InfoPokemonLoc addLocToInfoPoke(InfoPokemonLoc ipl, LocalisationPokemonView lpv)
+        {
+            ipl.TauxApp = lpv.TauxApp;
+            ipl.Lieu = lpv.Lieu;
+            return ipl;
+        }
+        public static InfoPokemonLoc addPokeToInfoPoke(InfoPokemonLoc ipl, PokemonView pv)
+        {
+            ipl.IdPokemon = pv.IdPokemon;
+            ipl.Nom = pv.Nom;
+            ipl.Img = pv.Img;
+            ipl.Types = TypeTools.TypeViewSimpleFromId(pv.IdPokemon);
+            return ipl;
+        }
+
+        public static List<InfoPokemonLoc> ListPokeLocToListInfo()
+        {
+            List<InfoPokemonLoc> listeIPL = new List<InfoPokemonLoc>();
+            return listeIPL;
+        }
+
+    }
+}
